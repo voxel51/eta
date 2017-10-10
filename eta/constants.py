b65a7b536bd8733f13c82469e430a290d8746771
brimoor <brimoor@umich.edu>
2017-10-09 20:55:37 -0700

    updating constants to not depend on any eta modules

--- eta/constants.py
+++ eta/constants.py
@@ -1,14 +1,15 @@
 '''
 ETA package-wide constants.
 
+IMPORTANT: this module should not import any ETA modules!
+
 Copyright 2017, Voxel51, LLC
 voxel51.com
 
 Brian Moore, brian@voxel51.com
 '''
+import json
 import os
-
-from core.config import Config
 
 
 # Directories
@@ -21,27 +22,13 @@ DEFAULT_CACHE_DIR = os.path.join(ETA_DIR, "cache")
 VERSION_JSON_PATH = os.path.join(ETA_DIR, "version.json")
 
 
-class _VersionConfig(Config):
-    '''Version info.'''
-
-    def __init__(self, d):
-        self.name = self.parse_string(d, "name")
-        self.version = self.parse_string(d, "version")
-        self.description = self.parse_string(d, "description")
-        self.author = self.parse_string(d, "author")
-        self.contact = self.parse_string(d, "contact")
-        self.url = self.parse_string(d, "url")
-        self.license = self.parse_string(d, "license")
-
-
-_version = _VersionConfig.from_json(VERSION_JSON_PATH)
-
-
-# Version info
-NAME = _version.name
-VERSION = _version.version
-DESCRIPTION = _version.description
-AUTHOR = _version.author
-CONTACT = _version.contact
-URL = _version.url
-LICENSE = _version.license
+# Version
+with open(VERSION_JSON_PATH) as f:
+    _version = json.load(f)
+NAME = str(_version["name"])
+VERSION = str(_version["version"])
+DESCRIPTION = str(_version["description"])
+AUTHOR = str(_version["author"])
+CONTACT = str(_version["contact"])
+URL = str(_version["url"])
+LICENSE = str(_version["license"])
