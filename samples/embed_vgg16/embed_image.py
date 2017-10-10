2d79e8010d6cd314832e9c2a63decbeeb0c67d6a
brimoor <brimoor@umich.edu>
2017-10-10 00:34:07 -0700

    linting and updating outpath to use samples/out directory used by other samples

--- samples/embed_vgg16/embed_image.py
+++ samples/embed_vgg16/embed_image.py
@@ -1,61 +1,64 @@
 #!/usr/bin/env python
 '''
-Example Code: Embed an image via VGG16.
-This example shows direct use of the VGG16 Class.
-The embed_video.py shows the use of the VideoFeaturization 
- classes that are the preferred approach for ETA modules
- since they maintain the on-disk backing store for the class
- which is used to communication between modules.
+ETA example image embbeding via VGG16.
 
-Also shows an example of starting up a tensor flow session.
+Note that the embed_video.py example shows the use of the VideoFeaturization
+classes, which is the preferred approach for ETA modules since they maintain
+the on-disk backing store for the class which is used to communication between
+modules.
+
+Also shows an example of starting a TensorFlow session.
 
 Copyright 2017, Voxel51, LLC
 voxel51.com
 
 Jason Corso, jjc@voxel51.com
 '''
-import errno
 import os
 import sys
 
 import tensorflow as tf
 import numpy as np
 
-from eta.core.config import Config
 import eta.core.image as im
-import eta.core.vgg16 as vgg
+from eta.core import utils
+from eta.core.vgg16 import VGG16
 
 
+def embed_image(impath):
+    '''Embeds the image using VGG16.
 
-def embed_image(image_path):
+    Uses the default weights specified in the default ETA config.
+
+    Stores the embedded vector as .npz, using VideoFeaturizer to handle I/O.
     '''
-        Uses the default weights specified in the default config.
-        Embeds the image the VGG16-net.  Store the embedded vectors as
-        a npz-->Uses a VideoFeaturizer to handle IO and storage on disk
-    '''
-
-    image = im.read(image_path)
-    if image is None:
-        print "could not load image: %s"%(image_path)
-        sys.exit(2)
+    img = im.read(impath)
 
     imgs = tf.placeholder(tf.float32, [None, 224, 224, 3])
     sess = tf.Session()
-    vggn = vgg.VGG16(imgs, sess)
+    vggn = VGG16(imgs, sess)
 
-    rmage = im.resize(image,224,224)
-    
-    embedded_vector = sess.run(vggn.fc2l, feed_dict={vggn.imgs: [rmage]})[0]
+    rimg = im.resize(img, 224, 224)
 
-    print "image embedded to vector of length %d"%(len(embedded_vector))
+    embedded_vector = sess.run(vggn.fc2l, feed_dict={vggn.imgs: [rimg]})[0]
+
+    print "image embedded to vector of length %d" % len(embedded_vector)
     print embedded_vector
-    np.savez_compressed('result_embed_image.npz',v=embedded_vector)
-    print "result saved to result_embed_image.npz"
+
+    outpath = _abspath("../out/result_embed_image.npz")
+    utils.ensure_dir(outpath)
+    np.savez_compressed(outpath, v=embedded_vector)
+    print "result saved to:", outpath
 
 
-if __name__ == '__main__':
-    impath = '../data/water.jpg'
+def _abspath(path):
+    return os.path.realpath(os.path.join(os.path.dirname(__file__), path))
+
+
+if __name__ == "__main__":
     if len(sys.argv) == 2:
         impath = sys.argv[1]
-    embed_image(impath)
+    else:
+        impath = _abspath("../data/water.jpg")
 
+    embed_image(impath)
