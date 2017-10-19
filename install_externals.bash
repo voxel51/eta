4769224240d7a60464506be001afb2e89ac50fcc
brimoor <brimoor@umich.edu>
2017-10-18 23:26:08 -0700

    adding flags to the install script to customize installation. for example, -b uses brew on mac and -p uses port

--- install_externals.bash
+++ install_externals.bash
@@ -1,15 +1,43 @@
 #!/bin/bash
 # Install external dependencies
 #
-# Usage:
-#   bash install_externals.bash [opencv-version]
-#
 # Copyright 2017, Voxel51, LLC
 # voxel51.com
 #
+# Brian Moore, brian@voxel51.com
+#
 
-#OPENCV_VERSION=${1:-2.4.13.3}
-OPENCV_VERSION=${1:-3.3.0}
+
+# Show usage information
+usage() {
+    echo "Usage:  bash $0 [-h] [-bp] [-u] [-v <opencv-version>]
+
+Options:
+     -h    Display this help message.
+     -b    Use brew to install packages (mac only). The default is false.
+     -p    Use port to install packages (mac only). The default is true.
+     -u    Update package manager before installing. The default is false.
+     -v    A specific OpenCV version to install. The default is 3.3.0."
+}
+
+
+# Parse flags
+SHOW_HELP=false
+USE_MACPORTS=true
+UPDATE_PACKAGES=false
+OPENCV_VERSION=3.3.0
+while getopts "hbpuv:" FLAG; do
+    case "${FLAG}" in
+        h) SHOW_HELP=true ;;
+        b) USE_MACPORTS=false ;;
+        p) USE_MACPORTS=true ;;
+        u) UPDATE_PACKAGES=true ;;
+        v) OPENCV_VERSION="${OPTARG}" ;;
+        *) usage ;;
+    esac
+done
+[ ${SHOW_HELP} = true ] && usage && exit 0
+
 
 CWD=`pwd`
 
@@ -23,72 +51,84 @@ rm -rf ${EXTERR}
 
 OS=`uname -s`
 
+set -o pipefail
 
-# Print message
-ECHO () {
-    printf "${1}\n"
-    printf "${1}\n" >> "${EXTLOG}"
+
+# Run command and print stdout/stderr to terminal and (separate) logs
+INFO () {
+    #"$@" >>${EXTLOG} 2>>${EXTERR}
+    ("$@" | tee -a ${EXTLOG}) 3>&1 1>&2 2>&3 | tee -a ${EXTERR}
 }
 
 
-# Run command and log stdout/stderr
-INFO () {
-    "$@" >>${EXTLOG} 2>>${EXTERR}
+# Print message
+MSG () {
+    INFO printf "***** ${1}\n"
+}
+
+
+# Exit by printing message and locations of log files
+EXIT () {
+    MSG "${1}"
+    MSG "Log file: ${EXTLOG}"
+    MSG "Error file: ${EXTERR}"
+    exit 0
 }
 
 
 # Run command, log stdout/stderr, and exit upon error
 CRITICAL () {
     INFO "$@"
-    if [ $? -ne 0 ]
-    then
-        ECHO "***** INSTALLATION FAILED WITH ERROR:"
-        cat ${EXTERR}
-        exit 1
+    if [ $? -ne 0 ]; then
+        EXIT "INSTALLATION FAILED"
     fi
 }
 
 
-ECHO "***** INSTALLATION STARTED"
-ECHO "Log file: ${EXTLOG}"
-ECHO "Error file: ${EXTERR}"
+MSG "INSTALLATION STARTED"
 
 
 # GPU flag
-ECHO "Checking system for GPU"
-if [ ${OS} == "Linux" ]
-then
+MSG "Checking system for GPU"
+if [ ${OS} == "Linux" ]; then
     CRITICAL lspci | grep -q "NVIDIA"
-    if [ $? -eq 0 ]
-    then
-      GCARD=ON
+    if [ $? -eq 0 ]; then
+        GCARD=ON
     else
-      GCARD=OFF
+        GCARD=OFF
     fi
-elif [ ${OS} == "Darwin" ]
-then
+elif [ ${OS} == "Darwin" ]; then
     GCARD=OFF
 fi
-ECHO "Setting GCARD=${GCARD}"
+MSG "Setting GCARD=${GCARD}"
 
 
-# Linux-specific items
-if [ ${OS} == "Linux" ]
-then
-    CRITICAL sudo apt-get -y install build-essential
+# Update package managers
+if [ ${UPDATE_PACKAGES} = true ]; then
+    if [ ${OS} == "Linux" ]; then
+        MSG "Installing build-essential"
+        CRITICAL sudo apt-get -y install build-essential
+    elif [ ${OS} == "Darwin" ]; then
+        if [ ${USE_MACPORTS} = true ]; then
+            MSG "Updating MacPorts"
+            CRITICAL sudo port selfupdate
+        else
+            MSG "Updating Homebrew"
+            CRITICAL brew update
+        fi
+    fi
 fi
 
 
 # Install python requirements
-ECHO "Installing Python packages"
+MSG "Installing Python packages"
 CRITICAL pip install -r requirements.txt
 
 
 # Tensorflow is also a requirement, but it depends on the GPU, so we install
 # that explicitly
-ECHO "Installing TensorFlow"
-if [ ${GCARD} == "ON" ]
-then
+MSG "Installing TensorFlow"
+if [ ${GCARD} == "ON" ]; then
     CRITICAL pip install tensorflow-gpu
 else
     CRITICAL pip install tensorflow
@@ -97,45 +137,46 @@ fi
 
 # ffmpeg
 INFO command -v ffmpeg
-if [ $? -eq 0 ]
-then
-    ECHO "ffmpeg already installed"
+if [ $? -eq 0 ]; then
+    MSG "ffmpeg already installed"
 else
-    ECHO "Installing ffmpeg"
-    if [ ${OS} == "Linux" ]
-    then
+    MSG "Installing ffmpeg"
+    if [ ${OS} == "Linux" ]; then
         CRITICAL sudo apt-get -y install ffmpeg
-    elif [ ${OS} == "Darwin" ]
-    then
-        CRITICAL brew install ffmpeg
+    elif [ ${OS} == "Darwin" ]; then
+        if [ ${USE_MACPORTS} = true ]; then
+            CRITICAL sudo port install ffmpeg
+        else
+            CRITICAL brew install ffmpeg
+        fi
     fi
 fi
 
 
 # imagemagick
 INFO command -v convert
-if [ $? -eq 0 ]
-then
-    ECHO "imagemagick already installed"
+if [ $? -eq 0 ]; then
+    MSG "imagemagick already installed"
 else
-    ECHO "Installing imagemagick"
-    if [ ${OS} == "Linux" ]
-    then
+    MSG "Installing imagemagick"
+    if [ ${OS} == "Linux" ]; then
         CRITICAL sudo apt-get -y install imagemagick
-    elif [ ${OS} == "Darwin" ]
-    then
-        CRITICAL brew install imagemagick
+    elif [ ${OS} == "Darwin" ]; then
+        if [ ${USE_MACPORTS} = true ]; then
+            CRITICAL sudo port install imagemagick
+        else
+            CRITICAL brew install imagemagick
+        fi
     fi
 fi
 
 
 # OpenCV
 INFO pkg-config --cflags opencv
-if [ $? -eq 0 ]
-then
-    ECHO "OpenCV already installed"
+if [ $? -eq 0 ]; then
+    MSG "OpenCV already installed"
 else
-    ECHO "Installing OpenCV ${OPENCV_VERSION}"
+    MSG "Installing OpenCV ${OPENCV_VERSION}"
 
     # Download source
     cd ${EXTDIR}
@@ -161,4 +202,4 @@ else
 fi
 
 
-ECHO "***** INSTALLATION COMPLETE"
+EXIT "INSTALLATION COMPLETE"
