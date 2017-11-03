e96ed6307f8bbdd4808139ae4c6e6fd2a66f618e
brimoor <brimoor@umich.edu>
2017-11-02 20:05:03 -0700

    updating numutils.py for py23 compatibility

--- eta/core/numutils.py
+++ eta/core/numutils.py
@@ -6,6 +6,12 @@ voxel51.com
 
 Jason Corso, jjc@voxel51.com
 '''
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from builtins import *
+
 import numpy as np
 
 
