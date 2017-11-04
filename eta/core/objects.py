86ebbdee2ba6bf9d8683b503c0e51d56ebd4de26
brimoor <brimoor@umich.edu>
2017-11-03 23:09:16 -0700

    adding pylint directives to avoid superfluous errors

--- eta/core/objects.py
+++ eta/core/objects.py
@@ -6,11 +6,17 @@ voxel51.com
 
 Brian Moore, brian@voxel51.com
 '''
+# pragma pylint: disable=redefined-builtin
+# pragma pylint: disable=unused-wildcard-import
+# pragma pylint: disable=wildcard-import
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 from builtins import *
+# pragma pylint: enable=redefined-builtin
+# pragma pylint: enable=unused-wildcard-import
+# pragma pylint: enable=wildcard-import
 
 from eta.core.geometry import BoundingBox
 from eta.core.serial import Serializable
