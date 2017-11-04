c87ea1d7ca08647eafff95e326a9fad87b5072d7
brimoor <brimoor@umich.edu>
2017-11-03 23:09:16 -0700

    adding pylint directives to avoid superfluous errors

--- eta/core/web.py
+++ eta/core/web.py
@@ -6,12 +6,18 @@ voxel51.com
 
 Brian Moore, brian@voxel51.com
 '''
+# pragma pylint: disable=redefined-builtin
+# pragma pylint: disable=unused-wildcard-import
+# pragma pylint: disable=wildcard-import
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 from builtins import *
 from future.utils import iteritems
+# pragma pylint: enable=redefined-builtin
+# pragma pylint: enable=unused-wildcard-import
+# pragma pylint: enable=wildcard-import
 
 import requests
 
