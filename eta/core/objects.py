a36485be880658637606aa63c6de7aa6f3efad60
brimoor <brimoor@umich.edu>
2017-12-07 22:44:48 -0800

    don't modify Frame in-place, and add ability to specify any or all

--- eta/core/objects.py
+++ eta/core/objects.py
@@ -41,17 +41,40 @@ class Frame(Serializable):
         '''
         self.objects.append(obj)
 
-    def matches_any(self, filters):
-        '''Keep detected objects that match at least one filter.
+    def label_set(self):
+        '''Returns a set containing the labels of objects in this frame.'''
+        return set(obj.label for obj in self.objects)
+
+    def get_matches(self, filters, match=any):
+        '''Returns a Frame containing only objects that match the filters.
 
         Args:
             filters: a list of functions that accept DetectedObjects and return
                 True/False
+            match: a function (usually any or all) that accepts an iterable and
+                returns True/False. Used to aggregate the outputs of each
+                filter to decide whether a match has occurred. The default is
+                any
         '''
-        self.objects = list(filter(
-            lambda o: any(f(o) for f in filters),
-            self.objects,
-        ))
+        return Frame(
+            objects=list(filter(
+                lambda o: match(f(o) for f in filters),
+                self.objects,
+            )),
+        )
+
+    def count_matches(self, filters, match=any):
+        '''Counts number of detected objects that match the filters.
+
+        Args:
+            filters: a list of functions that accept DetectedObjects and return
+                True/False
+            match: a function (usually any or all) that accepts an iterable and
+                returns True/False. Used to aggregate the outputs of each
+                filter to decide whether a match has occurred. The default is
+                any
+        '''
+        return len(self.get_matches(filters, match=match).objects)
 
     @classmethod
     def from_dict(cls, d):
