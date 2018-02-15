3728e290206442c8bcfd470a773090b35aac4b68
brimoor <brimoor@umich.edu>
2018-02-14 23:11:13 -0800

    loading config.json for global access from eta

--- eta/__init__.py
+++ eta/__init__.py
@@ -1,7 +1,7 @@
 '''
 ETA package initialization.
 
-Copyright 2017, Voxel51, LLC
+Copyright 2017-2018, Voxel51, LLC
 voxel51.com
 
 Brian Moore, brian@voxel51.com
@@ -19,10 +19,12 @@ from builtins import *
 # pragma pylint: enable=wildcard-import
 
 import logging
+import os
 
-import eta.constants as c
-from eta.core import log
-from eta.core import utils
+import eta.constants as etac
+from eta.core.config import Config
+import eta.core.log as etal
+import eta.core.utils as etau
 
 
 logger = logging.getLogger(__name__)
@@ -30,10 +32,26 @@ logger = logging.getLogger(__name__)
 
 def startup_message():
     '''Logs ETA startup message.'''
-    logger.info("Starting...\n" + c.ASCII_ART)
-    logger.info("%s %s, %s", c.NAME, c.VERSION, c.AUTHOR)
-    logger.info("Revision %s\n", utils.get_eta_rev())
+    logger.info("Starting...\n" + etac.ASCII_ART)
+    logger.info("%s %s, %s", etac.NAME, etac.VERSION, etac.AUTHOR)
+    logger.info("Revision %s\n", etau.get_eta_rev())
+
+
+class EtaConfig(Config):
+    '''Sytem-wide ETA configuration settings.'''
+
+    def __init__(self, d):
+        self.module_dirs = self.parse_array(d, "module_dirs", default=[])
 
 
 # Default logging behavior
-log.basic_setup()
+etal.basic_setup()
+
+
+# Load config
+# @todo allow configs to be overridden by environment variables
+if os.path.isfile(etac.CONFIG_JSON_PATH):
+    config = EtaConfig.from_json(etac.CONFIG_JSON_PATH)
+else:
+    logger.warning("Could not find config '%s'", etac.CONFIG_JSON_PATH)
+    config = EtaConfig.default()
