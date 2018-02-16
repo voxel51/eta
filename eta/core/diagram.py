22bff28b4e6fae234aa3ee50cfbed0d376b4be3e
brimoor <brimoor@umich.edu>
2018-02-16 00:30:21 -0800

    implementing pipeline diagram building and improving module diagram implementation

--- eta/core/diagram.py
+++ eta/core/diagram.py
@@ -1,5 +1,5 @@
 '''
-Core utilities for building block diagrams of modules and pipelines
+Core tools for building block diagrams of modules and pipelines.
 
 Copyright 2017-2018, Voxel51, LLC
 voxel51.com
@@ -19,6 +19,7 @@ from future.utils import iteritems, itervalues
 # pragma pylint: enable=unused-wildcard-import
 # pragma pylint: enable=wildcard-import
 
+import copy
 import logging
 import os
 
@@ -28,8 +29,10 @@ import eta.core.utils as etau
 logger = logging.getLogger(__name__)
 
 
-BLOCKDIAG_EXECUTABLE = "blockdiag"
-BLOCKDIAG_INDENTATION = 2
+EXECUTABLE = "blockdiag"
+INDENTATION = 2
+TOP_LEVEL_NAME = "blockdiag"
+GROUP_NAME = "group"
 PARAM_COLOR = "\"#EE7531\""  # Voxel51 orange
 MODULE_COLOR = "\"#AAAAAA\""  # Voxel51 gray
 MODULE_HEIGHT = 60
@@ -39,8 +42,8 @@ NODE_HEIGHT = 40
 class BlockDiagram(object):
     '''Mixin class for objects that can be rendered as block diagrams.'''
 
-    def _to_blockdiag(self, path):
-        raise NotImplementedError("subclass must implement _to_blockdiag()")
+    def to_blockdiag(self):
+        raise NotImplementedError("subclass must implement to_blockdiag()")
 
     def render(self, path):
         '''Render the block diagram as an SVG image.
@@ -57,11 +60,11 @@ class BlockDiagram(object):
 
         # Generate block diagram
         logger.info("Generating blockdiag file '%s'", blockdiag_path)
-        self._to_blockdiag(blockdiag_path)
+        self.to_blockdiag().write(blockdiag_path)
 
         # Render SVG
         logger.info("Rendering block diagram image '%s'", svg_path)
-        args = [BLOCKDIAG_EXECUTABLE, "-Tsvg", "-o", svg_path, blockdiag_path]
+        args = [EXECUTABLE, "-Tsvg", "-o", svg_path, blockdiag_path]
         etau.communicate_or_die(args)
 
 
@@ -71,20 +74,22 @@ class BlockdiagFile(object):
     def __init__(self):
         '''Creates a BlockdiagFile instance.'''
         self._file = BlockdiagGroup(is_top_level=True)
-        self._modules = BlockdiagSection(comment="modules")
-        self._inputs = BlockdiagSection(comment="inputs")
-        self._outputs = BlockdiagSection(comment="outputs")
-        self.parameters = BlockdiagSection(comment="parameters")
-        self._io_edges = BlockdiagSection(comment="I/O connections")
-        self._param_edges = BlockdiagSection(comment="parameter connections")
-        self._param_groups = {}
 
-        self._file.add_element(self._modules)
-        self._file.add_element(self._inputs)
-        self._file.add_element(self._outputs)
-        self._file.add_element(self.parameters)
-        self._file.add_element(self._io_edges)
-        self._file.add_element(self._param_edges)
+    def add_element(self, element):
+        '''Adds the element to the file.'''
+        self._file.add_element(element)
+
+    def export(self, **kwargs):
+        '''Exports the file as a BlockdiagGroup.
+
+        Args:
+            kwargs: an optional dictionary of key = value attributes to add to
+                the returned group.
+        '''
+        group = copy.deepcopy(self._file)
+        group.name = GROUP_NAME  # downgrade from top-level to nested group
+        group.add_attributes(**kwargs)
+        return group
 
     def write(self, path):
         '''Writes the block diagram to disk, creating the output directory if
@@ -94,42 +99,94 @@ class BlockdiagFile(object):
         with open(path, "wb") as f:
             f.write(self._file.render())
 
-    def _add_module(self, name, shape="box"):
-        node = BlockdiagNode(
-            name, shape=shape,
-            height=MODULE_HEIGHT, width=_module_extent(name))
-        self._modules.add_element(node)
 
-        group = BlockdiagGroup(orientation="portrait", color=PARAM_COLOR)
-        self._param_edges.add_element(group)
-        self._param_groups[name] = group
+class BlockdiagPipeline(BlockdiagFile):
+    '''Class representing a pipeline as a blockdiag file.'''
 
-    def _add_input(self, name, shape="endpoint"):
+    def __init__(self, name):
+        '''Creates a new block diagram pipeline.
+
+        Args:
+            name: the name of the pipeline
+        '''
+        self.name = name
+        super(BlockdiagPipeline, self).__init__()
+
+        self._inputs = BlockdiagSection(comment="inputs")
+        self._outputs = BlockdiagSection(comment="outputs")
+        self._connections = BlockdiagSection(comment="connections")
+        self._modules = BlockdiagSection(comment="modules")
+        self._module_prefixes = {}
+
+        self.add_element(self._inputs)
+        self.add_element(self._outputs)
+        self.add_element(self._connections)
+        self.add_element(self._modules)
+
+    def add_module(self, name, blockdiag_module):
+        '''Adds the module to the pipeline.
+
+        Args:
+            name: a unique name for the module
+            blockdiag_module: a BlockdiagModule instance
+        '''
+        group = blockdiag_module.export(color=MODULE_COLOR)
+        # Prefix the module names to ensure uniqueness between modules
+        group.prefix_names(self._get_prefix(name))
+        self._modules.add_element(group)
+
+    def add_input(self, name):
+        '''Adds an input to the pipeline.
+
+        Args:
+            name (str): the input name
+        '''
         node = BlockdiagNode(
-            name, shape=shape, height=NODE_HEIGHT, width=_node_extent(name))
+            name, shape="cloud", height=NODE_HEIGHT,
+            width=_node_extent(name))
         self._inputs.add_element(node)
 
-    def _add_output(self, name, shape="endpoint"):
+    def add_output(self, name):
+        '''Adds an output to the pipeline.
+
+        Args:
+            name (str): the output name
+        '''
         node = BlockdiagNode(
-            name, shape=shape, height=NODE_HEIGHT, width=_node_extent(name))
+            name, shape="cloud", height=NODE_HEIGHT,
+            width=_node_extent(name))
         self._outputs.add_element(node)
 
-    def _add_parameter(self, name, shape="beginpoint"):
-        node = BlockdiagNode(
-            name, shape=shape, rotate=270, height=_node_extent(name),
-            width=NODE_HEIGHT)
-        self.parameters.add_element(node)
+    def add_connection(self, source, sink):
+        '''Adds a connection (directed edge) between the given nodes.
 
-    def _add_io_edge(self, source, sink):
-        self._io_edges.add_element(BlockdiagDirectedEdge(source, sink))
+        Args:
+            source (PipelineNode): the source of the edge
+            sink (PipelineNode): the sink of the edge
+        '''
+        # Apply name prefixes
+        src = source.field
+        snk = sink.field
+        if source.is_module_input or source.is_module_output:
+            src = self._get_prefix(source.module) + src
+        if sink.is_module_input or sink.is_module_output:
+            snk = self._get_prefix(sink.module) + snk
 
-    def _add_param_edge(self, param_name, module_name):
-        self._param_groups[module_name].add_element(
-            BlockdiagDirectedEdge(param_name, module_name))
+        edge = BlockdiagDirectedEdge(src, snk)
+        self._connections.add_element(edge)
+
+    def _get_prefix(self, module_name):
+        try:
+            prefix = self._module_prefixes[module_name]
+        except KeyError:
+            prefix = "%d." % (len(self._module_prefixes) + 1)
+            self._module_prefixes[module_name] = prefix
+
+        return prefix
 
 
 class BlockdiagModule(BlockdiagFile):
-    '''Class representing a blockdiag module.'''
+    '''Class representing a module as a blockdiag file.'''
 
     def __init__(self, name):
         '''Creates a new block diagram module.
@@ -137,24 +194,69 @@ class BlockdiagModule(BlockdiagFile):
         Args:
             name: the name of the module
         '''
-        super(BlockdiagModule, self).__init__()
         self.name = name
+        super(BlockdiagModule, self).__init__()
+
+        self._module = BlockdiagSection(comment="module")
+        self._inputs = BlockdiagSection(comment="inputs")
+        self._outputs = BlockdiagSection(comment="outputs")
+        self._parameters = BlockdiagSection(comment="parameters")
+        self._io_edges = BlockdiagSection(comment="I/O connections")
+        self._param_edges = BlockdiagSection(comment="parameter connections")
+
         self._add_module(name)
+
+        self.add_element(self._module)
+        self.add_element(self._inputs)
+        self.add_element(self._outputs)
+        self.add_element(self._parameters)
+        self.add_element(self._io_edges)
+        self.add_element(self._param_edges)
 
     def add_input(self, name):
         '''Add a module input with the given name.'''
-        self._add_input(name)
+        node = BlockdiagNode(
+            name, shape="endpoint", height=NODE_HEIGHT,
+            width=_node_extent(name))
+        self._inputs.add_element(node)
+
         self._add_io_edge(name, self.name)
 
     def add_output(self, name):
         '''Add a module output with the given name.'''
-        self._add_output(name)
+        node = BlockdiagNode(
+            name, shape="endpoint", height=NODE_HEIGHT,
+            width=_node_extent(name))
+        self._outputs.add_element(node)
+
         self._add_io_edge(self.name, name)
 
     def add_parameter(self, name):
         '''Add a module parameter with the given name.'''
-        self._add_parameter(name)
-        self._add_param_edge(name, self.name)
+        node = BlockdiagNode(
+            name, shape="beginpoint", rotate=270, height=_node_extent(name),
+            width=NODE_HEIGHT)
+        self._parameters.add_element(node)
+
+        self._add_param_edge(name)
+
+    def _add_module(self, name):
+        node = BlockdiagNode(
+            name, shape="box",
+            height=MODULE_HEIGHT, width=_module_extent(name))
+        self._module.add_element(node)
+
+        self._param_group = BlockdiagGroup(
+            orientation="portrait", color=PARAM_COLOR)
+        self._param_edges.add_element(self._param_group)
+
+    def _add_io_edge(self, source, sink):
+        edge = BlockdiagDirectedEdge(source, sink)
+        self._io_edges.add_element(edge)
+
+    def _add_param_edge(self, param_name):
+        edge = BlockdiagDirectedEdge(param_name, self.name)
+        self._param_group.add_element(edge)
 
 
 class BlockdiagElement(object):
@@ -181,6 +283,19 @@ class BlockdiagContainer(BlockdiagElement):
         '''Returns True/False if the container is empty.'''
         return len(self.elements) == 0
 
+    def prefix_names(self, prefix):
+        '''Prepend the given prefix to all nodes names in the given container.
+        Operates recursively on nested containers.
+        '''
+        for e in self.elements:
+            if isinstance(e, BlockdiagContainer):
+                e.prefix_names(prefix)
+            elif isinstance(e, BlockdiagNode):
+                e.name = prefix + e.name
+            elif isinstance(e, BlockdiagDirectedEdge):
+                e.source = prefix + e.source
+                e.sink = prefix + e.sink
+
     def add_element(self, element):
         '''Adds the element to the container.'''
         self.elements.append(element)
@@ -199,19 +314,27 @@ class BlockdiagGroup(BlockdiagContainer):
                 the group
         '''
         super(BlockdiagGroup, self).__init__()
-        self.name = "blockdiag" if is_top_level else "group"
-        self.attributes = [
-            BlockdiagAttribute(k, v) for k, v in iteritems(kwargs)
-        ]
+        self.name = TOP_LEVEL_NAME if is_top_level else GROUP_NAME
+        self.attributes = []
+        self.add_attributes(**kwargs)
+
+    def add_attributes(self, **kwargs):
+        '''Adds the given attributes to the group.
+
+        Args:
+            kwwargs: a dictionary of key = value attributes
+        '''
+        for k, v in iteritems(kwargs):
+            self.attributes.append(BlockdiagAttribute(k, v))
 
     def render(self, indent=0):
         if self.is_empty:
             return ""
         c = [_indent(self.name + " {", indent)]
         for a in self.attributes:
-            c.append(a.render(indent=indent + BLOCKDIAG_INDENTATION))
+            c.append(a.render(indent=indent + INDENTATION))
         for e in self.elements:
-            c.append(e.render(indent=indent + BLOCKDIAG_INDENTATION))
+            c.append(e.render(indent=indent + INDENTATION))
         c.append(_indent("}", indent))
         return "\n".join(c).rstrip()
 
