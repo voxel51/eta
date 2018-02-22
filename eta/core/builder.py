25fe98418b697cf7062fae6f62bcc6488a299961
brimoor <brimoor@umich.edu>
2018-02-22 02:21:01 -0800

    initial pipeline building implementation. needs a few tweaks

--- eta/core/builder.py
+++ eta/core/builder.py
@@ -19,9 +19,162 @@ from future.utils import iteritems
 # pragma pylint: enable=unused-wildcard-import
 # pragma pylint: enable=wildcard-import
 
+import logging
+import os
+import time
 
+import eta
 from eta.core.config import Config, Configurable
+import eta.core.job as etaj
+import eta.core.log as etal
+import eta.core.module as etam
 import eta.core.pipeline as etap
+import eta.core.types as etat
+
+
+logger = logging.getLogger(__name__)
+
+
+PIPELINE_JSON_FILE = "pipeline.json"
+PIPELINE_LOG_FILE = "pipeline.log"
+MODULE_CONFIG_EXT = ".config"
+
+
+def _get_timestamp():
+    return time.strftime("%Y.%m.%d-%H:%M:%S")
+
+
+class PipelineBuilder(object):
+    '''Class that handles building a pipeline based on a PipelineRequest.'''
+
+    def __init__(self, request):
+        '''Creates a PipelineBuilder instance.
+
+        Args:
+            request: a PipelineRequest instance
+        '''
+        self.request = request
+
+        self.timestamp = None
+        self.config_dir = ""
+        self.output_dir = ""
+        self.pipeline_config = None
+        self.pipeline_config_path = ""
+        self.module_configs = {}
+        self.module_config_paths = {}
+
+    def build(self):
+        '''Builds the pipeline and writes the associated config files.'''
+        self.timestamp = _get_timestamp()
+        self.config_dir = self._get_config_dir()
+        self.output_dir = self._get_output_dir()
+
+        self._build_pipeline_config()
+        self._build_module_configs()
+
+    def _build_pipeline_config(self):
+        # Build job configs
+        # @todo handle non-py executables
+        jobs = []
+        for module in self.request.metadata.execution_order:
+            metadata = self.request.metadata.modules[module].metadata
+            jobs.append(
+                etaj.JobConfig.builder().set(
+                    name=module,
+                    working_dir=".",
+                    script=etam.find_exe(metadata.info.exe),
+                    config_path=self._get_module_config_path(module),
+                ).build()
+            )
+
+        # Build logging config
+        logging_config = etal.LoggingConfig.builder().set(
+            filename=self._get_log_path()
+        ).build()
+
+        # Build pipeline config
+        self.pipeline_config = etap.PipelineConfig.builder().set(
+            name=self.request.name,
+            working_dir=".",
+            overwrite=False,
+            jobs=jobs,
+            logging_config=logging_config,
+        ).build()
+
+        # Write pipeline config
+        self.pipeline_config_path = self._get_pipeline_config_path()
+        logger.info("Writing pipeline config '%s'", self.pipeline_config_path)
+        self.pipeline_config.write_json(self.pipeline_config_path)
+
+    def _build_module_configs(self):
+        for module in self.request.metadata.execution_order:
+            metadata = self.request.metadata.modules[module].metadata
+
+            # Build field configs
+            inputs = {}
+            outputs = {}
+            parameters = {}
+            # @todo implement this
+
+            # Build module config
+            module_config = GenericModuleConfig.builder().set(
+                inputs=GenericFieldConfig(inputs),
+                outputs=GenericFieldConfig(outputs),
+                parameters=GenericFieldConfig(parameters),
+            ).build()
+
+            # Write module config
+            module_config_path = self._get_module_config_path(module)
+            logger.info("Writing module config '%s'", module_config_path)
+            module_config.write_json(module_config_path)
+
+            self.module_configs[module] = module_config
+            self.module_config_paths[module] = module_config_path
+
+    def _get_module_config_path(self, module):
+        return os.path.join(self.config_dir, module + MODULE_CONFIG_EXT)
+
+    def _get_pipeline_config_path(self):
+        return os.path.join(self.config_dir, PIPELINE_JSON_FILE)
+
+    def _get_log_path(self):
+        return os.path.join(self.config_dir, PIPELINE_LOG_FILE)
+
+    def _get_data_path(self, field, params):
+        return field.type.gen_path(self.output_dir, params)
+
+    def _get_config_dir(self):
+        return os.path.join(
+            eta.config.config_dir,
+            self.request.name + os.sep + self.timestamp
+        )
+
+    def _get_output_dir(self):
+        return os.path.join(
+            eta.config.output_dir,
+            self.request.name + os.sep + self.timestamp
+        )
+
+
+class GenericModuleConfig(etam.BaseModuleConfig):
+    '''Generic module configuration class.'''
+
+    def __init__(self, d):
+        super(GenericModuleConfig, self).__init__(d)
+        self.inputs = self.parse_object(
+            d, "inputs", GenericFieldConfig, default=GenericFieldConfig())
+        self.outputs = self.parse_dict(
+            d, "outputs", GenericFieldConfig, default=GenericFieldConfig())
+        self.parameters = self.parse_dict(
+            d, "parameters", GenericFieldConfig, default=GenericFieldConfig())
+
+
+class GenericFieldConfig(Config):
+    '''Generic module field configuration class.'''
+
+    def __init__(self, d):
+        for k, v in iteritems(d):
+            setattr(self, k, v)
 
 
 class PipelineRequestConfig(Config):
