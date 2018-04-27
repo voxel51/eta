ce3e8a3a6ad46fc9af6cfa7fc2c15960f55ce46e
brimoor <brimoor@umich.edu>
2018-04-27 08:29:18 -0700

    updating docstring

--- docs/guide2tex.bash
+++ docs/guide2tex.bash
@@ -5,14 +5,17 @@
 #   All content in the dev guide before the first second-level (##) or greater
 #   heading is omitted
 #
+# Installation:
+#   https://github.com/jgm/pandoc/releases
+#
 # Usage:
 #   bash guide2tex.bash modules_dev_guide.md
+#   bash guide2tex.bash pipelines_dev_guide.md
 #
-# Voxel51, LLC, Copyright 2017
+# Voxel51, LLC, Copyright 2017-2018
 # voxel51.com
 #
 # Brian Moore, brian@voxel51.com
-# Jason Corso, jjc@voxel51.com
 #
 
 MDPATH=${1}
