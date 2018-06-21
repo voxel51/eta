c1259d7f6e3a83e1721debba84a4cd279aac0829
brimoor <brimoor@umich.edu>
2018-06-20 19:42:28 -0700

    updating constants

--- eta/__init__.py
+++ eta/__init__.py
@@ -42,8 +42,11 @@ class ETAConfig(EnvConfig):
             d, "module_dirs", env_var="ETA_MODULE_DIRS", default=[])
         self.pipeline_dirs = self.parse_string_array(
             d, "pipeline_dirs", env_var="ETA_PIPELINE_DIRS", default=[])
-        self.weights_dirs = self.parse_string_array(
-            d, "weights_dirs", env_var="ETA_WEIGHTS_DIRS", default=[])
+        self.models_dirs = self.parse_string_array(
+            d, "models_dirs", env_var="ETA_MODELS_DIRS", default=[])
+        self.max_model_versions_to_keep = self.parse_string(
+            d, "max_model_versions_to_keep",
+            env_var="ETA_MAX_MODEL_VERSIONS_TO_KEEP", default="")
         self.default_sequence_idx = self.parse_string(
             d, "default_sequence_idx", env_var="ETA_DEFAULT_SEQUENCE_IDX",
             default="%05d")
