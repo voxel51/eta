12cf758ef2266b25b6e60356288bfb6006a10e98
Jason Corso <jason@voxel51.com>
2018-06-28 16:28:08 -0400

    first part of adding Container to serial

--- eta/core/serial.py
+++ eta/core/serial.py
@@ -193,6 +193,187 @@ class Serializable(object):
         return cls.from_dict(read_json(path))
 
 
+class Container(Serializable):
+    '''Base class for flexible containers that store lists of serializable
+    elements.
+
+    This class should not be instantiated directly. Instead a subclass should
+    be created for each type of data to be stored.  The two common direct
+    sub-classes of this `Container` are `DataContainer` and `ConfigContainer`
+    to store data and `Config` instances respectively.  See these two class
+    definitions for examples of using `Container`.
+
+    By default, Container subclasses embed their class names and underlying
+    data instance class names in their JSON representations, so data
+    containers can be read reflectively from disk.
+
+    Attributes:
+        <element>: a list of elements. The field name <element> is specified
+            by the `_ELE_ATTR` member, and the class of the elements is
+            specified by the `_ELE_CLS` member
+    '''
+
+    # The class of the element stored in the container
+    _ELE_CLS = None
+
+    # The name of the attribute that will store the elements in the container
+    _ELE_ATTR = "elements"
+
+    def __init__(self, **kwargs):
+        '''Constructs a Container.
+
+        Args:
+            <element>: an optional list of elements to store in the container.
+            The appropriate name of this keyword argument is determined by the
+            `_ELE_ATTR` member of the container class.  If a subclass chooses
+            not to override this attribute, then the default keyword is
+            "elements"
+
+        Raises:
+            ContainerError: if there was a problem parsing the input
+        '''
+        self._validate()
+
+        if kwargs and self._ELE_ATTR not in kwargs:
+            raise ContainerError(
+                "Expected elements to be provided in keyword argument '%s'; "
+                "found keys %s" % (self._ELE_ATTR, list(kwargs.keys())))
+        elements = kwargs.get(self._ELE_ATTR, [])
+
+        for e in elements:
+            if not isinstance(e, self._ELE_CLS):
+                raise ContainerError(
+                    "Container %s expects elements of type %s but found "
+                    "%s" % (self.__class__, self._ELE_CLS, e.__class__))
+
+        setattr(self, self._ELE_ATTR, elements)
+
+    def __getitem__(self, index):
+        return self._elements.__getitem__(index)
+
+    def __iter__(self):
+        return iter(self._elements)
+
+    @property
+    def _elements(self):
+        '''Convenient access function to get the list of elements stored
+        independent of what the name of that attribute is.
+        '''
+        return getattr(self, self._ELE_ATTR)
+
+    @classmethod
+    def _validate(cls):
+        if cls._DATA_CLS is None:
+            raise DataContainerError(
+                "_DATA_CLS is None; note that you cannot instantiate "
+                "DataContainer directly.")
+        if cls._DATA_ATTR is None:
+            raise DataContainerError(
+                "_DATA_ATTR is None; note that you cannot instantiate "
+                "DataContainer directly.")
+
+    def add(self, instance):
+        '''Adds a data instance to the container.
+
+        Args:
+            instance: an instance of `_DATA_CLS`
+        '''
+        self._data.append(instance)
+
+    def attributes(self):
+        return ["_CLS", "_DATA_CLS", self._DATA_ATTR]
+
+    def count_matches(self, filters, match=any):
+        '''Counts number of data instances that match the filters.
+
+        Args:
+            filters: a list of functions that accept data instances and return
+                True/False
+            match: a function (usually `any` or `all`) that accepts an iterable
+                and returns True/False. Used to aggregate the outputs of each
+                filter to decide whether a match has occurred. The default is
+                `any`
+        '''
+        return self.get_matches(filters, match=match).num
+
+    @classmethod
+    def from_dict(cls, d):
+        '''Constructs an DataContainer from a JSON dictionary.
+
+        If the JSON contains the reflective `_CLS` and `_DATA_CLS` fields, they
+        are used to infer the underlying data classes, and this method can
+        be invoked as `DataContainer.from_dict`. Otherwise, this method must
+        be called on a concrete subclass of `DataContainer`.
+        '''
+        if "_CLS" in d and "_DATA_CLS" in d:
+            # Parse reflectively
+            cls = etau.get_class(d["_CLS"])
+            data_cls = etau.get_class(d["_DATA_CLS"])
+        else:
+            # Parse using provided class
+            cls._validate()
+            data_cls = cls._DATA_CLS
+        return cls(**{
+            cls._DATA_ATTR:
+            [data_cls.from_dict(dd) for dd in d[cls._DATA_ATTR]]
+        })
+
+    @classmethod
+    def get_class_name(cls):
+        '''Returns the fully-qualified class name string of this container.'''
+        return etau.get_class_name(cls)
+
+    @classmethod
+    def get_data_class(cls):
+        '''Gets the class of data instances stored in this container.'''
+        return cls._DATA_CLS
+
+    @classmethod
+    def get_data_class_name(cls):
+        '''Returns the fully-qualified class name string of the data in this
+        container.
+        '''
+        return etau.get_class_name(cls._DATA_CLS)
+
+    def get_matches(self, filters, match=any):
+        '''Returns a data container containing only instances that match the
+        filters.
+
+        Args:
+            filters: a list of functions that accept data instances and return
+                True/False
+            match: a function (usually `any` or `all`) that accepts an iterable
+                and returns True/False. Used to aggregate the outputs of each
+                filter to decide whether a match has occurred. The default is
+                `any`
+        '''
+        return self.__class__(**{
+            self._DATA_ATTR:
+            list(filter(lambda o: match(f(o) for f in filters), self._data))
+        })
+
+    @property
+    def size(self):
+        '''Returns the number of data instances in the container.'''
+        return len(self._data)
+
+    def serialize(self):
+        '''Custom serialization implementation for DataContainers that embeds
+        the class name, and the data class name in the JSON to enable
+        reflective parsing when reading from disk.
+        '''
+        d = OrderedDict()
+        d["_CLS"] = self.get_class_name()
+        d["_DATA_CLS"] = self.get_data_class_name()
+        d[self._DATA_ATTR] = [o.serialize() for o in self._data]
+        return d
+
+
+class ContainerError(Exception):
+    '''Exception raised when an invalid Container is encountered.'''
+    pass
+
+
 def is_serializable(obj):
     '''Returns True if the object is serializable (i.e., implements the
     Serializable interface) and False otherwise.
