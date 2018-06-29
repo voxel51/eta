55f8ac2990f41129b8bd4cff329a285123caf582
brimoor <brimoor@umich.edu>
2018-06-28 23:19:44 -0700

    linting

--- eta/core/c3d.py
+++ eta/core/c3d.py
@@ -117,69 +117,69 @@ class C3D(object):
     def _build_conv_layers(self):
         with tf.name_scope("conv1") as scope:
             weights = self._variable_with_weight_decay(
-                    "wc1", [3, 3, 3, 3, 64], 0.04, 0.00)
+                "wc1", [3, 3, 3, 3, 64], 0.04, 0.00)
             biases = self._variable_with_weight_decay(
-                    "bc1", [64], 0.04, 0.0)
+                "bc1", [64], 0.04, 0.0)
             conv = self._conv3d(self.clips, weights, biases)
             self.conv1 = tf.nn.relu(conv, name=scope)
             self.pool1 = self._max_pool("pool1", self.conv1, k=1)
 
         with tf.name_scope("conv2") as scope:
             weights = self._variable_with_weight_decay(
-                    "wc2", [3, 3, 3, 64, 128], 0.04, 0.00)
+                "wc2", [3, 3, 3, 64, 128], 0.04, 0.00)
             biases = self._variable_with_weight_decay(
-                    "bc2", [128], 0.04, 0.0)
+                "bc2", [128], 0.04, 0.0)
             conv = self._conv3d(self.pool1, weights, biases)
             self.conv2 = tf.nn.relu(conv, name=scope)
             self.pool2 = self._max_pool("pool2", self.conv2, k=2)
 
         with tf.name_scope("conv3a") as scope:
             weights = self._variable_with_weight_decay(
-                    "wc3a", [3, 3, 3, 128, 256], 0.04, 0.00)
+                "wc3a", [3, 3, 3, 128, 256], 0.04, 0.00)
             biases = self._variable_with_weight_decay(
-                    "bc3a", [256], 0.04, 0.0)
+                "bc3a", [256], 0.04, 0.0)
             conv = self._conv3d(self.pool2, weights, biases)
             self.conv3a = tf.nn.relu(conv, name=scope)
 
         with tf.name_scope("conv3b") as scope:
             weights = self._variable_with_weight_decay(
-                    "wc3b", [3, 3, 3, 256, 256], 0.04, 0.00)
+                "wc3b", [3, 3, 3, 256, 256], 0.04, 0.00)
             biases = self._variable_with_weight_decay(
-                    "bc3b", [256], 0.04, 0.0)
+                "bc3b", [256], 0.04, 0.0)
             conv = self._conv3d(self.conv3a, weights, biases)
             self.conv3b = tf.nn.relu(conv, name=scope)
             self.pool3 = self._max_pool("pool3", self.conv3b, k=2)
 
         with tf.name_scope("conv4a") as scope:
             weights = self._variable_with_weight_decay(
-                    "wc4a", [3, 3, 3, 256, 512], 0.04, 0.00)
+                "wc4a", [3, 3, 3, 256, 512], 0.04, 0.00)
             biases = self._variable_with_weight_decay(
-                    "bc4a", [512], 0.04, 0.0)
+                "bc4a", [512], 0.04, 0.0)
             conv = self._conv3d(self.pool3, weights, biases)
             self.conv4a = tf.nn.relu(conv, name=scope)
 
         with tf.name_scope("conv4b") as scope:
             weights = self._variable_with_weight_decay(
-                    "wc4b", [3, 3, 3, 512, 512], 0.04, 0.00)
+                "wc4b", [3, 3, 3, 512, 512], 0.04, 0.00)
             biases = self._variable_with_weight_decay(
-                    "bc4b", [512], 0.04, 0.0)
+                "bc4b", [512], 0.04, 0.0)
             conv = self._conv3d(self.conv4a, weights, biases)
             self.conv4b = tf.nn.relu(conv, name=scope)
             self.pool4 = self._max_pool("pool4", self.conv4b, k=2)
 
         with tf.name_scope("conv5a") as scope:
             weights = self._variable_with_weight_decay(
-                    "wc5a", [3, 3, 3, 512, 512], 0.04, 0.00)
+                "wc5a", [3, 3, 3, 512, 512], 0.04, 0.00)
             biases = self._variable_with_weight_decay(
-                    "bc5a", [512], 0.04, 0.0)
+                "bc5a", [512], 0.04, 0.0)
             conv = self._conv3d(self.pool4, weights, biases)
             self.conv5a = tf.nn.relu(conv, name=scope)
 
         with tf.name_scope("conv5b") as scope:
             weights = self._variable_with_weight_decay(
-                    "wc5b", [3, 3, 3, 512, 512], 0.04, 0.00)
+                "wc5b", [3, 3, 3, 512, 512], 0.04, 0.00)
             biases = self._variable_with_weight_decay(
-                    "bc5b", [512], 0.04, 0.0)
+                "bc5b", [512], 0.04, 0.0)
             conv = self._conv3d(self.conv5a, weights, biases)
             self.conv5b = tf.nn.relu(conv, name=scope)
             self.pool5 = self._max_pool("pool5", self.conv5b, k=2)
@@ -188,27 +188,27 @@ class C3D(object):
         with tf.name_scope("fc1") as scope:
             inputs = tf.reshape(self.pool5, [1, 8192])
             weights = self._variable_with_weight_decay(
-                    "wd1", [8192, 4096], 0.04, 0.001)
+                "wd1", [8192, 4096], 0.04, 0.001)
             biases = self._variable_with_weight_decay(
-                    "bd1", [4096], 0.04, 0.0)
+                "bd1", [4096], 0.04, 0.0)
             activations = tf.nn.relu(
                 tf.matmul(inputs, weights) + biases, name=scope)
             self.fc1 = tf.nn.dropout(activations, 0.6)
 
         with tf.name_scope("fc2") as scope:
             weights = self._variable_with_weight_decay(
-                    "wd2", [4096, 4096], 0.04, 0.002)
+                "wd2", [4096, 4096], 0.04, 0.002)
             biases = self._variable_with_weight_decay(
-                    "bd2", [4096], 0.04, 0.0)
+                "bd2", [4096], 0.04, 0.0)
             self.fc2l = tf.matmul(self.fc1, weights) + biases
             activations = tf.nn.relu(self.fc2l, name=scope)
             self.fc2 = tf.nn.dropout(activations, 0.6)
 
         with tf.name_scope("fc3") as scope:
             weights = self._variable_with_weight_decay(
-                    "wout", [4096, 101], 0.04, 0.005)
+                "wout", [4096, 101], 0.04, 0.005)
             biases = self._variable_with_weight_decay(
-                    "bout", [101], 0.04, 0.0)
+                "bout", [101], 0.04, 0.0)
             self.fc3 = tf.matmul(self.fc2, weights) + biases
 
     def _build_output_layer(self):
@@ -234,10 +234,10 @@ class C3DFeaturizer(Featurizer):
     '''Featurizer that embeds videos into the C3D feature space.'''
 
     def __init__(self, config=None):
-        super(C3DFeaturizer,self).__init__()
+        super(C3DFeaturizer, self).__init__()
         self.config = config or C3DFeaturizerConfig.default()
         self.validate(self.config)
-        self.c3d  = None
+        self.c3d = None
 
     def dim(self):
         '''The dimension of the features extracted by this Featurizer.'''
