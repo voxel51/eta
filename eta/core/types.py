976cb8d05c97375cf938991ba1468d741508c5d6
brimoor <brimoor@umich.edu>
2019-03-10 13:47:51 -0400

    adding types for ImageLabels

--- eta/core/types.py
+++ eta/core/types.py
@@ -771,6 +771,30 @@ class DetectedObjectsSequence(JSONFileSequence):
     pass
 
 
+class ImageLabels(JSONFile):
+    '''A description of the labeled contents of an image.
+
+    This type is implemented in ETA by the `eta.core.image.ImageLabels`
+    class.
+
+    Examples:
+        /path/to/image_labels.json
+    '''
+    pass
+
+
+class ImageSetLabels(JSONFile):
+    '''A description of the labeled contents of a set of images.
+
+    This type is implemented in ETA by the `eta.core.image.ImageSetLabels`
+    class.
+
+    Examples:
+        /path/to/image_set_labels.json
+    '''
+    pass
+
+
 class VideoLabels(JSONFile):
     '''A description of the labeled contents of a video.
 
