f436396d4f2847ecd20083e2739e36aee6da7ebc
brimoor <brimoor@umich.edu>
2019-03-23 22:07:22 -0700

    overhauling Classifier interface structure, and adding support for video frames classifiers

--- eta/core/learning.py
+++ eta/core/learning.py
@@ -20,6 +20,7 @@ from builtins import *
 # pragma pylint: enable=wildcard-import
 
 from eta.core.config import Config, Configurable
+import eta.core.data as etad
 
 
 class ClassifierConfig(Config):
@@ -50,21 +51,156 @@ class ClassifierConfig(Config):
 class Classifier(Configurable):
     '''Interface for classifiers.
 
-    Subclasses of Classifier must implement the `predict()` method.
+    Subclasses of `Classifier` must implement the `predict()` method.
+
+    Subclasses can optionally implement the context manager interface to
+    perform any necessary setup and teardown.
     '''
 
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        pass
+
     def predict(self, arg):
-        '''Peforms prediction on the given input.
+        '''Peforms prediction on the given argument.
 
         Args:
-            arg: the input to classify. Depending on the classifier, this may
-                be an image, video, or embedding(s)
+            arg: the data to classify
 
         Returns:
             an `eta.core.data.AttributeContainer` describing the predictions
-                for the input
         '''
-        raise NotImplementedError("subclass must implement predict()")
+        raise NotImplementedError("subclasses must implement predict()")
+
+
+class ImageClassifier(Classifier):
+    '''Base class for all classifiers that operate on single images.
+
+    `ImageClassifier`s may output single or multiple labels per image.
+
+    Subclasses of `ImageClassifier` must implement the `predict()` method, and
+    they can optionally provide custom (efficient) implementations of the
+    `predict_all()` method.
+
+    Subclasses can optionally implement the context manager interface to
+    perform any necessary setup and teardown, e.g., operating a `Featurizer`
+    that featurizes the input images.
+    '''
+
+    def predict(self, img):
+        '''Peforms prediction on the given image.
+
+        Args:
+            img: the image to classify
+
+        Returns:
+            an `eta.core.data.AttributeContainer` instance containing the
+                predictions
+        '''
+        raise NotImplementedError("subclasses must implement predict()")
+
+    def predict_all(self, imgs):
+        '''Performs prediction on the given tensor of images.
+
+        Subclasses can override this method to increase efficiency, but, by
+        default, this method simply iterates over the images and predicts each.
+
+        Args:
+            imgs: a list (or d x ny x nx x 3 tensor) of images to classify
+
+        Returns:
+            a list of `eta.core.data.AttributeContainer` instances describing
+                the predictions for each image
+        '''
+        return [self.predict(img) for img in imgs]
+
+
+class VideoFramesClassifier(Classifier):
+    '''Base class for all classifiers that operate directly on videos
+    represented as tensors of images.
+
+    `VideoFramesClassifier`s may output single or multiple labels per video
+    clip.
+
+    Subclasses of `VideoFramesClassifier` must implement the `predict()` method.
+
+    Subclasses can optionally implement the context manager interface to
+    perform any necessary setup and teardown, e.g., operating a `Featurizer`
+    that featurizes the input frames.
+    '''
+
+    def predict(self, imgs):
+        '''Peforms prediction on the given video represented as a tensor of
+        images.
+
+        Args:
+            imgs: a list (or d x ny x nx x 3 tensor) of images defining the
+                video to classify
+
+        Returns:
+            an `eta.core.data.AttributeContainer` instance describing
+                the predictions for the input
+        '''
+        raise NotImplementedError("subclasses must implement predict()")
+
+
+class VideoFramesVotingClassifierConfig(Config):
+    '''Configuration settings for a VideoFramesVotingClassifier.'''
+
+    def __init__(self, d):
+        self.image_classifier = self.parse_object(
+            d, "image_classifier", ClassifierConfig)
+        self.confidence_weighted_vote = self.parse_bool(
+            d, "confidence_weighted_vote", default=False)
+
+
+class VideoFramesVotingClassifier(VideoFramesClassifier):
+    '''A video frames classifier that uses an `ImageClassifier` to classify
+    each image and then votes on each attribute to determine the predictions
+    for the video.
+
+    Note that all attributes are combined into a single vote. Thus, even if the
+    `ImageClassifier` is a multilabel classifier, each prediction will contain
+    the single most prevelant label.
+    '''
+
+    def __init__(self, config):
+        '''Creates a VideoFramesVotingClassifier instance.
+
+        Args:
+            config: a VideoFramesVotingClassifierConfig instance
+        '''
+        self.config = config
+        self.image_classifier = config.image_classifier.build()
+
+        if not issubclass(self.image_classifier, ImageClassifier):
+            raise ValueError("image_classifier must be a %s", ImageClassifier)
+
+    def __enter__(self):
+        self.image_classifier.__enter__()
+        return self
+
+    def __exit__(self, *args):
+        self.image_classifier.__exit__(*args)
+
+    def predict(self, imgs):
+        '''Peforms prediction on the given video represented as a tensor of
+        images.
+
+        Args:
+            imgs: a list (or d x ny x nx x 3 tensor) of images defining the
+                video to classify
+
+        Returns:
+            an `eta.core.data.AttributeContainer` instance describing
+                the predictions for the input
+        '''
+        frame_attrs = self.image_classifier.predict_all(imgs)
+        return etad.majority_vote_categorical_attrs(
+            frame_attrs,
+            confidence_weighted=self.config.confidence_weighted_vote)
 
 
 class ObjectDetectorConfig(Config):
