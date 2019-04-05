cdfbcb9a890c9333fe0475235ff3a21040bc0b66
brimoor <brimoor@umich.edu>
2019-04-04 23:58:50 -0400

    updating copyright

--- eta/core/utils.py
+++ eta/core/utils.py
@@ -1,11 +1,11 @@
 '''
 Core system and file I/O utilities.
 
-Copyright 2017-2018, Voxel51, Inc.
+Copyright 2017-2019, Voxel51, Inc.
 voxel51.com
 
 Brian Moore, brian@voxel51.com
-Jason Corso, jjc@voxel51.com
+Jason Corso, jason@voxel51.com
 '''
 # pragma pylint: disable=redefined-builtin
 # pragma pylint: disable=unused-wildcard-import
