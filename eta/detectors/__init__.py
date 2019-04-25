b7482605e905649c8dd36edce8e375c006d89def
brimoor <brimoor@umich.edu>
2019-04-25 00:08:53 -0400

    adding TFModelDetector implementation to wrap tfmodel object detectors

---
+++ eta/detectors/__init__.py
@@ -0,0 +1,8 @@
+'''
+Package declaration.
+
+Copyright 2017-2019, Voxel51, Inc.
+voxel51.com
+
+Brian Moore, brian@voxel51.com
+'''
---
+++ eta/detectors/tfmodels/__init__.py
@@ -0,0 +1,10 @@
+'''
+Package declaration.
+
+Copyright 2017-2019, Voxel51, Inc.
+voxel51.com
+
+Brian Moore, brian@voxel51.com
+'''
+
+from tfmodels_detectors import *
