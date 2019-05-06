bf4bd4216b91d5ef81d1553fb275b6df8cd378ec
brimoor <brimoor@umich.edu>
2019-05-06 01:49:57 -0400

    renaming classify_video to apply_video_classifier, for consistency

--- eta/modules/apply_video_classifier.py
+++ eta/modules/apply_video_classifier.py
@@ -35,7 +35,7 @@ import eta.core.video as etav
 logger = logging.getLogger(__name__)
 
 
-class ClassifyVideoConfig(etam.BaseModuleConfig):
+class ApplyVideoClassifierConfig(etam.BaseModuleConfig):
     '''Module configuration settings.
 
     Attributes:
@@ -44,7 +44,7 @@ class ClassifyVideoConfig(etam.BaseModuleConfig):
     '''
 
     def __init__(self, d):
-        super(ClassifyVideoConfig, self).__init__(d)
+        super(ApplyVideoClassifierConfig, self).__init__(d)
         self.data = self.parse_object_array(d, "data", DataConfig)
         self.parameters = self.parse_object(d, "parameters", ParametersConfig)
 
@@ -74,7 +74,7 @@ class ParametersConfig(Config):
 
     Parameters:
         classifier (eta.core.types.VideoClassifier): an
-            `eta.core.learning.ClassifierConfig` describing the
+            `eta.core.learning.VideoClassifierConfig` describing the
             `eta.core.learning.VideoClassifier` to use
         confidence_threshold (eta.core.types.Number): [None] a confidence
             threshold to use when assigning labels
@@ -82,7 +82,7 @@ class ParametersConfig(Config):
 
     def __init__(self, d):
         self.classifier = self.parse_object(
-            d, "classifier", etal.ClassifierConfig)
+            d, "classifier", etal.VideoClassifierConfig)
         self.confidence_threshold = self.parse_number(
             d, "confidence_threshold", default=None)
 
@@ -102,12 +102,10 @@ def _build_attribute_filter(threshold):
     return filter_fcn
 
 
-def _classify_video(config):
+def _apply_video_classifier(config):
     # Build classifier
     classifier = config.parameters.classifier.build()
     logger.info("Loaded classifier %s", type(classifier))
-    if not isinstance(classifier, etal.VideoClassifier):
-        raise ValueError("Classifier must be a %s" % etal.VideoClassifier)
 
     # Process videos
     with classifier:
@@ -128,23 +126,23 @@ def _process_video(data, classifier, parameters):
     attr_filter = _build_attribute_filter(parameters.confidence_threshold)
 
     logger.info("Classifying video '%s'", data.video_path)
-    attrs = attr_filter(classifier.predict(data.video_path))
-    labels.add_video_attributes(attrs)
+    attrs = classifier.predict(data.video_path)
+    labels.add_video_attributes(attr_filter(attrs))
 
     logger.info("Writing labels to '%s'", data.output_labels_path)
     labels.write_json(data.output_labels_path)
 
 
 def run(config_path, pipeline_config_path=None):
-    '''Run the classify_video module.
+    '''Run the apply_video_classifier module.
 
     Args:
-        config_path: path to a ClassifyVideoConfig file
+        config_path: path to a ApplyVideoClassifierConfig file
         pipeline_config_path: optional path to a PipelineConfig file
     '''
-    config = ClassifyVideoConfig.from_json(config_path)
+    config = ApplyVideoClassifierConfig.from_json(config_path)
     etam.setup(config, pipeline_config_path=pipeline_config_path)
-    _classify_video(config)
+    _apply_video_classifier(config)
 
 
 if __name__ == "__main__":
