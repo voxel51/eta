d0c761339f8dbdb56409ccd7ef2186496fc64d31
brimoor <brimoor@umich.edu>
2019-05-06 01:46:40 -0400

    adding ImageClassifier wrapper for VGG16

---
+++ eta/classifiers/__init__.py
@@ -0,0 +1,11 @@
+'''
+Package declaration.
+
+Copyright 2017-2019, Voxel51, Inc.
+voxel51.com
+
+Brian Moore, brian@voxel51.com
+'''
+
+# Import all classifiers into the `eta.classifiers` namespace
+from vgg16 import VGG16Classifier, VGG16ClassifierConfig
