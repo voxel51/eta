eda4bb18d8af9113b063c203f947a63275da5a25
brimoor <brimoor@umich.edu>
2019-05-19 19:28:23 -0700

    using python 3 compatible relative imports

--- eta/classifiers/__init__.py
+++ eta/classifiers/__init__.py
@@ -8,6 +8,6 @@ Brian Moore, brian@voxel51.com
 '''
 
 # Import all classifiers into the `eta.classifiers` namespace
-from vgg16_classifiers import VGG16Classifier, VGG16ClassifierConfig
-from voting_classifiers import VideoFramesVotingClassifier, \
-                               VideoFramesVotingClassifierConfig
+from .vgg16_classifiers import VGG16Classifier, VGG16ClassifierConfig
+from .voting_classifiers import VideoFramesVotingClassifier, \
+                                VideoFramesVotingClassifierConfig
