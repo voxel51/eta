0ddbe45a2ccc5772898d55e399a3b0d29f0f26be
brimoor <brimoor@umich.edu>
2019-05-23 12:30:10 -0700

    updating TFModelsDetector to use UsesTFSession mixin

--- eta/detectors/tfmodels/tfmodels_detectors.py
+++ eta/detectors/tfmodels/tfmodels_detectors.py
@@ -32,6 +32,7 @@ from eta.core.learning import ObjectDetector
 import eta.core.models as etam
 from eta.core.objects import DetectedObject, DetectedObjectContainer
 import eta.core.serial as etas
+from eta.core.tfutils import UsesTFSession
 import eta.core.utils as etau
 
 sys.path.append(os.path.join(etac.TF_OBJECT_DETECTION_DIR, "utils"))
@@ -62,27 +63,34 @@ class TFModelsDetectorConfig(Config):
         return cls.from_json(DEFAULT_CONFIG)
 
 
-class TFModelsDetector(ObjectDetector):
-    '''Interface to the `tensorflow/models` object detection library.
-
+class TFModelsDetector(ObjectDetector, UsesTFSession):
+    '''Interface to the TF-Models object detection library at
     https://github.com/tensorflow/models/tree/master/research/object_detection.
+
+    This class uses `eta.core.tfutils.UsesTFSession` to create TF sessions, so
+    it automatically applies settings in your `eta.config.tf_config`.
+
+    Instances of this class must either use the context manager interface or
+    manually call `close()` when finished to release memory.
     '''
+
 
     def __init__(self, config=None):
         '''Constructs a TFModelsDetector instance.
 
         Args:
-            config: an optional TFModelsDetectorConfig instance. If absent, the
-                default TFModelsDetectorConfig will be used
+            config: an optional TFModelsDetectorConfig instance. If omitted,
+                the default TFModelsDetectorConfig will be used
         '''
         self.config = config or TFModelsDetectorConfig.load_default()
+        UsesTFSession.__init__(self)
 
         # Only downloads the model if necessary
         model_path = etam.download_model(self.config.model_name)
 
         # Load model
         self._tf_graph = self._build_graph(model_path)
-        self._sess = tf.Session(graph=self._tf_graph)
+        self._sess = self.make_tf_session(graph=self._tf_graph)
 
         # Load labels
         label_map = gool.load_labelmap(self.config.labels_path)
@@ -90,15 +98,11 @@ class TFModelsDetector(ObjectDetector):
             label_map, max_num_classes=90, use_display_name=True)
         self._category_index = gool.create_category_index(categories)
 
-    @staticmethod
-    def _build_graph(model_path):
-        tf_graph = tf.Graph()
-        with tf_graph.as_default():
-            graph_def = tf.GraphDef()
-            with tf.gfile.GFile(model_path, "rb") as f:
-                graph_def.ParseFromString(f.read())
-                tf.import_graph_def(graph_def, name="")
-        return tf_graph
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        self.close()
 
     def detect(self, img):
         '''Performs detection on the input image.
@@ -123,6 +127,16 @@ class TFModelsDetector(ObjectDetector):
             for b, s, c in zip(boxes, scores, classes)
         ]
         return DetectedObjectContainer(objects=objects)
+
+    @staticmethod
+    def _build_graph(model_path):
+        tf_graph = tf.Graph()
+        with tf_graph.as_default():
+            graph_def = tf.GraphDef()
+            with tf.gfile.GFile(model_path, "rb") as f:
+                graph_def.ParseFromString(f.read())
+                tf.import_graph_def(graph_def, name="")
+        return tf_graph
 
 
 def _to_detected_object(box, score, class_id, label_map):
