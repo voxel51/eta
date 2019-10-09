f94c1a63255a406cf7e19d72da2c9efab348060d
Ian Timmis <ian@voxel51.com>
2019-10-09 15:29:16 -0400

    Update install.bash
    Removed the manual installation instructions for protoc from the install script.

--- install.bash
+++ install.bash
@@ -235,22 +235,6 @@ MSG "Initializing submodules"
 CRITICAL git submodule init
 CRITICAL git submodule update
 
-#
-# Install tensorflow/models
-#
-# This requires compiling protocol buffers, which, in turn requires a suitable
-# version of `protoc` to be installed on your machine. ETA's `requirements.txt`
-# file attempted to install `protoc`, but, if that didn't work, then the code
-# below will try to install it another way.
-#
-# NOTE: If you don't have proctoc>=3.6.1, you may get errors when the `protoc`
-# command below runs. To manually install a recent protoc version, follow these
-# instructions:
-# https://gist.github.com/sofyanhadia/37787e5ed098c97919b8c593f0ec44d8
-#
-# As of this writing, we recommend installing protoc==3.6.1. Prebuilt binary:
-# https://github.com/google/protobuf/releases/download/v3.6.1/protoc-3.6.1-linux-x86_64.zip
-#
 MSG "Installing tensorflow/models"
 cd tensorflow/models
 INFO command -v protoc
