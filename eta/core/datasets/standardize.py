c24dd3b2805b860ad7821fd4ce68fde4b2bb031e
Tyler Ganter <tyler@voxel51.com>
2020-01-29 16:39:08 -0500

    dataset function

--- eta/core/datasets/standardize.py
+++ eta/core/datasets/standardize.py
@@ -133,3 +133,56 @@ def check_dataset_syntax(dataset, target_schema, audit_only=True):
     )
 
     return checker.fixable_schema, checker.unfixable_schema
+
+
+def check_duplicate_attrs(dataset, video_attr_multi_value_names=None,
+                          img_or_frame_attr_multi_value_names=None,
+                          obj_attr_multi_value_names=None,
+                          event_attr_multi_value_names=None):
+    '''Check for duplicate attributes (and return boolean)
+
+    Args:
+        dataset: a `LabeledDataset` instance
+        video_attr_multi_value_names: list of attr name strings that the
+            video attrs is allowed to have multiple DIFFERENT values for
+        img_or_frame_attr_multi_value_names: same for image/frame attrs
+        obj_attr_multi_value_names: same for object attrs
+        event_attr_multi_value_names: same for event attrs
+
+    Returns:
+        integer count of labels files containing duplicate attributes
+    '''
+    logger.info("Checking for duplicate attrs for labeled dataset...")
+
+    dup_attrs_count = 0
+
+    for idx, labels_path in enumerate(dataset.iter_labels_paths()):
+        if idx % 20 == 0:
+            logger.info("%4d/%4d" % (idx, len(dataset)))
+
+        labels = dataset.read_labels(labels_path)
+
+        if isinstance(labels, etai.ImageLabels):
+            has_duplicates = labels.has_duplicate_attrs(
+                img_or_frame_attr_multi_value_names,
+                obj_attr_multi_value_names
+            )
+        elif isinstance(labels, etav.VideoLabels):
+            has_duplicates = labels.has_duplicate_attrs(
+                video_attr_multi_value_names,
+                img_or_frame_attr_multi_value_names,
+                obj_attr_multi_value_names,
+                event_attr_multi_value_names
+            )
+        else:
+            raise ValueError(
+                "Unexpected labels type: '%s'" % etau.get_class_name(labels))
+
+        dup_attrs_count += int(has_duplicates)
+
+    logger.info(
+        "Complete: %d/%d files have duplicate attributes"
+        % (dup_attrs_count, len(dataset))
+    )
+
+    return dup_attrs_count
