bcf32803cfec1a2c8ce828e7cd4054eeee72e4e2
brimoor <brimoor@umich.edu>
2020-02-13 01:37:31 -0500

    making FrameLabels a proper base class for other frame label types, complete with a Schema

--- eta/core/frames.py
+++ eta/core/frames.py
@@ -21,40 +21,39 @@ import six
 
 import numpy as np
 
-from eta.core.data import AttributeContainer
-from eta.core.objects import DetectedObjectContainer
-from eta.core.serial import Serializable
+import eta.core.data as etad
+import eta.core.labels as etal
+import eta.core.objects as etao
+import eta.core.serial as etas
 import eta.core.utils as etau
 
 
-class FrameLabels(Serializable):
-    '''Class encapsulating labels for a specific frame.
+class FrameLabels(etal.Labels):
+    '''Class encapsulating labels for a frame, i.e., an image or a specific
+    frame of a video.
 
     Attributes:
-        frame_number: frame number
         attrs: AttributeContainer describing attributes of the frame
         objects: DetectedObjectContainer describing detected objects in the
             frame
     '''
 
-    def __init__(self, frame_number, attrs=None, objects=None):
-        '''Constructs a `FrameLabels` instance.
+    def __init__(self, attrs=None, objects=None):
+        '''Constructs a FrameLabels instance.
 
         Args:
-            frame_number: the frame number
             attrs: (optional) AttributeContainer of attributes for the frame.
                 By default, an empty AttributeContainer is created
             objects: (optional) DetectedObjectContainer of detected objects for
                 the frame. By default, an empty DetectedObjectContainer is
                 created
         '''
-        self.frame_number = frame_number
-        self.attrs = attrs or AttributeContainer()
-        self.objects = objects or DetectedObjectContainer()
+        self.attrs = attrs or etad.AttributeContainer()
+        self.objects = objects or etao.DetectedObjectContainer()
 
     @property
-    def has_frame_attributes(self):
-        '''Whether the frame has at least one frame attribute.'''
+    def has_attributes(self):
+        '''Whether the frame has at least one attribute.'''
         return bool(self.attrs)
 
     @property
@@ -65,9 +64,9 @@ class FrameLabels(Serializable):
     @property
     def is_empty(self):
         '''Whether the frame has no labels of any kind.'''
-        return not self.has_frame_attributes and not self.has_objects
+        return not self.has_attributes and not self.has_objects
 
-    def add_frame_attribute(self, frame_attr):
+    def add_attribute(self, frame_attr):
         '''Adds the attribute to the frame.
 
         Args:
@@ -75,7 +74,7 @@ class FrameLabels(Serializable):
         '''
         self.attrs.add(frame_attr)
 
-    def add_frame_attributes(self, frame_attrs):
+    def add_attributes(self, frame_attrs):
         '''Adds the attributes to the frame.
 
         Args:
@@ -101,24 +100,24 @@ class FrameLabels(Serializable):
 
     def clear(self):
         '''Removes all labels from the frame.'''
-        self.clear_frame_attributes()
+        self.clear_attributes()
         self.clear_objects()
 
-    def clear_frame_attributes(self):
-        '''Removes all frame attributes from the frame.'''
-        self.attrs = AttributeContainer()
+    def clear_attributes(self):
+        '''Removes all attributes from the frame.'''
+        self.attrs = etad.AttributeContainer()
 
     def clear_objects(self):
         '''Removes all objects from the frame.'''
-        self.objects = DetectedObjectContainer()
+        self.objects = etao.DetectedObjectContainer()
 
-    def merge_frame_labels(self, frame_labels):
+    def merge_labels(self, frame_labels):
         '''Merges the labels into the frame.
 
         Args:
             frame_labels: a FrameLabels
         '''
-        self.add_frame_attributes(frame_labels.attrs)
+        self.add_attributes(frame_labels.attrs)
         self.add_objects(frame_labels.objects)
 
     def remove_objects_without_attrs(self, labels=None):
@@ -137,27 +136,12 @@ class FrameLabels(Serializable):
         Returns:
             a list of attribute names
         '''
-        _attrs = ["frame_number"]
+        _attrs = []
         if self.attrs:
             _attrs.append("attrs")
         if self.objects:
             _attrs.append("objects")
         return _attrs
-
-    @classmethod
-    def from_image_labels(cls, image_labels, frame_number):
-        '''Constructs a `FrameLabels` from an `ImageLabels`.
-
-        Args:
-            image_labels: an ImageLabels instance
-            frame_number: the frame number
-
-        Returns:
-            a FrameLabels instance
-        '''
-        return cls(
-            frame_number, attrs=image_labels.attrs,
-            objects=image_labels.objects)
 
     @classmethod
     def from_dict(cls, d):
@@ -171,13 +155,364 @@ class FrameLabels(Serializable):
         '''
         attrs = d.get("attrs", None)
         if attrs is not None:
-            attrs = AttributeContainer.from_dict(attrs)
+            attrs = etad.AttributeContainer.from_dict(attrs)
 
         objects = d.get("objects", None)
         if objects is not None:
-            objects = DetectedObjectContainer.from_dict(objects)
+            objects = etao.DetectedObjectContainer.from_dict(objects)
 
-        return cls(d["frame_number"], attrs=attrs, objects=objects)
+        return cls(attrs=attrs, objects=objects)
+
+
+class FrameLabelsSchema(etal.LabelsSchema):
+    '''Schema for FrameLabels.
+
+    Attributes:
+        attrs: an AttributeContainerSchema describing the attributes of the
+            frame(s)
+        objects: an ObjectContainerSchema describing the objects of the
+            frame(s)
+    '''
+
+    def __init__(self, attrs=None, objects=None):
+        '''Creates a FrameLabelsSchema instance.
+
+        Args:
+            attrs: (optional) an AttributeContainerSchema describing the
+                attributes of the frame(s)
+            objects: (optional) an ObjectContainerSchema describing the objects
+                of the frame(s)
+        '''
+        self.attrs = attrs or etad.AttributeContainerSchema()
+        self.objects = objects or etao.ObjectContainerSchema()
+
+    def has_attribute(self, attr_name):
+        '''Whether the schema has an attribute with the given name.
+
+        Args:
+            attr_name: an attribute name
+
+        Returns:
+            True/False
+        '''
+        return self.attrs.has_attribute(attr_name)
+
+    def get_attribute_class(self, attr_name):
+        '''Gets the `Attribute` class for the attribute with the given name.
+
+        Args:
+            attr_name: an attribute name
+
+        Returns:
+            the Attribute class
+        '''
+        return self.attrs.get_attribute_class(attr_name)
+
+    def has_object_label(self, label):
+        '''Whether the schema has an object with the given label.
+
+        Args:
+            label: an object label
+
+        Returns:
+            True/False
+        '''
+        return self.objects.has_object_label(label)
+
+    def get_object_schema(self, label):
+        '''Gets the `ObjectSchema` for the object with the given label.
+
+        Args:
+            label: the object label
+
+        Returns:
+            the ObjectSchema
+        '''
+        return self.objects.get_object_schema(label)
+
+    def has_object_attribute(self, label, attr_name):
+        '''Whether the schema has an object with the given label with a
+        frame-level attribute with the given name.
+
+        Args:
+            label: an object label
+            attr_name: a frame-level object attribute name
+
+        Returns:
+            True/False
+        '''
+        return self.objects.has_frame_attribute(label, attr_name)
+
+    def get_object_attribute_schema(self, label, attr_name):
+        '''Gets the `AttributeSchema` for the frame-level attribute of the
+        given name for the object with the given label.
+
+        Args:
+            label: the object label
+            attr_name: the name of the frame-level object attribute
+
+        Returns:
+            the AttributeSchema
+        '''
+        return self.objects.get_frame_attribute_schema(label, attr_name)
+
+    def get_object_attribute_class(self, label, attr_name):
+        '''Gets the `Attribute` class for the frame-level attribute of the
+        given name for the object with the given label.
+
+        Args:
+            label: an object label
+            attr_name: a frame-level object attribute name
+
+        Returns:
+            the Attribute class
+        '''
+        return self.objects.get_frame_attribute_class(label, attr_name)
+
+    def add_attribute(self, attr):
+        '''Adds the given attribute to the schema.
+
+        Args:
+            attr: an Attribute
+        '''
+        self.attrs.add_attribute(attr)
+
+    def add_attributes(self, attrs):
+        '''Adds the given attributes to the schema.
+
+        Args:
+            attrs: an AttributeContainer
+        '''
+        self.attrs.add_attributes(attrs)
+
+    def add_object_label(self, label):
+        '''Adds the given object label to the schema.
+
+        Args:
+            label: an object label
+        '''
+        self.objects.add_object_label(label)
+
+    def add_object_attribute(self, label, attr):
+        '''Adds the frame-level attribute for the object with the given label
+        to the schema.
+
+        Args:
+            label: an object label
+            attr: a frame-level Attribute
+        '''
+        self.objects.add_frame_attribute(label, attr)
+
+    def add_object_attributes(self, label, attrs):
+        '''Adds the frame-level attributes for the object with the given label
+        to the schema.
+
+        Args:
+            label: an object label
+            attrs: a frame-level AttributeContainer
+        '''
+        self.objects.add_frame_attributes(label, attrs)
+
+    def add_object(self, obj):
+        '''Adds the object to the schema.
+
+        Args:
+            obj: a DetectedObject
+        '''
+        self.objects.add_object(obj)
+
+    def add_objects(self, objects):
+        '''Adds the objects to the schema.
+
+        Args:
+            objects: a DetectedObjectContainer
+        '''
+        self.objects.add_objects(objects)
+
+    def add_labels(self, frame_labels):
+        '''Adds the FrameLabels to the schema.
+
+        Args:
+            frame_labels: a FrameLabels
+        '''
+        self.add_attributes(frame_labels.attrs)
+        self.add_objects(frame_labels.objects)
+
+    def is_valid_attribute(self, attr):
+        '''Whether the attribute is compliant with the schema.
+
+        Args:
+            attr: an Attribute
+
+        Returns:
+            True/False
+        '''
+        return self.attrs.is_valid_attribute(attr)
+
+    def is_valid_object_label(self, label):
+        '''Whether the object label is compliant with the schema.
+
+        Args:
+            label: an object label
+
+        Returns:
+            True/False
+        '''
+        return self.objects.is_valid_object_label(label)
+
+    def is_valid_object_attribute(self, label, attr):
+        '''Whether the frame-level attribute for the object with the given
+        label is compliant with the schema.
+
+        Args:
+            label: an object label
+            attr: a frame-level Attribute
+
+        Returns:
+            True/False
+        '''
+        return self.objects.is_valid_frame_attribute(label, attr)
+
+    def is_valid_object(self, obj):
+        '''Whether the given object is compliant with the schema.
+
+        Args:
+            obj: a DetectedObject
+
+        Returns:
+            True/False
+        '''
+        return self.objects.is_valid_object(obj)
+
+    def validate_attribute(self, attr):
+        '''Validates that the attribute is compliant with the schema.
+
+        Args:
+            attr: an Attribute
+
+        Raises:
+            AttributeContainerSchemaError: if the attribute violates the schema
+        '''
+        self.attrs.validate_attribute(attr)
+
+    def validate_object_label(self, label):
+        '''Validates that the object label is compliant with the schema.
+
+        Args:
+            label: an object label
+
+        Raises:
+            ObjectContainerSchemaError: if the object label violates the schema
+        '''
+        self.objects.validate_object_label(label)
+
+    def validate_object_attribute(self, label, attr):
+        '''Validates that the frame-level attribute for the object with the
+        given label is compliant with the schema.
+
+        Args:
+            label: an object label
+            attr: a frame-level Attribute
+
+        Raises:
+            ObjectContainerSchemaError: if the object label violates the schema
+            AttributeContainerSchemaError: if the frame-level attribute
+                violates the schema
+        '''
+        self.objects.validate_frame_attribute(label, attr)
+
+    def validate_object(self, obj):
+        '''Validates that the object is compliant with the schema.
+
+        Args:
+            obj: a DetectedObject
+
+        Raises:
+            ObjectContainerSchemaError: if the object label violates the schema
+            AttributeContainerSchemaError: if any attributes of the object
+                violate the schema
+        '''
+        self.objects.validate_object(obj)
+
+    def validate(self, frame_labels):
+        '''Validates that the FrameLabels are compliant with the schema.
+
+        Args:
+            frame_labels: a FrameLabels instance
+
+        Raises:
+            AttributeContainerSchemaError: if a frame/object attribute
+                violates the schema
+            ObjectContainerSchemaError: if an object label violates the schema
+        '''
+        for attr in frame_labels.attrs:
+            self.validate_attribute(attr)
+
+        for obj in frame_labels.objects:
+            self.validate_object(obj)
+
+    def merge_schema(self, schema):
+        '''Merges the given FrameLabelsSchema into this schema.
+
+        Args:
+            schema: an FrameLabelsSchema
+        '''
+        self.attrs.merge_schema(schema.attrs)
+        self.objects.merge_schema(schema.objects)
+
+    @classmethod
+    def build_active_schema(cls, frame_labels):
+        '''Builds a FrameLabelsSchema that describes the active schema of the
+        given FrameLabels.
+
+        Args:
+            frame_labels: a FrameLabels instance
+
+        Returns:
+            a FrameLabelsSchema
+        '''
+        schema = cls()
+        schema.add_labels(frame_labels)
+        return schema
+
+    def attributes(self):
+        '''Returns the list of class attributes that will be serialized.
+
+        Returns:
+            a list of attribute names
+        '''
+        _attrs = []
+        if self.attrs:
+            _attrs.append("attrs")
+        if self.objects:
+            _attrs.append("objects")
+
+        return _attrs
+
+    @classmethod
+    def from_dict(cls, d):
+        '''Constructs a FrameLabelsSchema from a JSON dictionary.
+
+        Args:
+            d: a JSON dictionary
+
+        Returns:
+            an FrameLabelsSchema
+        '''
+        attrs = d.get("attrs", None)
+        if attrs is not None:
+            attrs = etad.AttributeContainerSchema.from_dict(attrs)
+
+        objects = d.get("objects", None)
+        if objects is not None:
+            objects = etao.ObjectContainerSchema.from_dict(objects)
+
+        return cls(attrs=attrs, objects=objects)
+
+
+class FrameLabelsSchemaError(etal.LabelsSchemaError):
+    '''Error raised when an `FrameLabelsSchema` is violated.'''
+    pass
 
 
 def frame_number_to_timestamp(frame_number, total_frame_count, duration):
@@ -293,7 +628,7 @@ def parse_frame_ranges(frames):
     return frame_ranges
 
 
-class FrameRanges(Serializable):
+class FrameRanges(etas.Serializable):
     '''Class representing a monotonically increasing and disjoint series of
     frames.
     '''
@@ -640,7 +975,7 @@ class FrameRangesError(Exception):
     pass
 
 
-class FrameRange(Serializable):
+class FrameRange(etas.Serializable):
     '''Class representing a range of frames.'''
 
     def __init__(self, first, last):
