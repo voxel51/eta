c2db376c3ecbbf3aa7a5734aa30c92344c3dd5a0
brimoor <brimoor@umich.edu>
2020-02-13 02:52:25 -0500

    updating copyright

--- eta/core/serial.py
+++ eta/core/serial.py
@@ -1,7 +1,7 @@
 '''
 Core data structures for working with data that can be read/written to disk.
 
-Copyright 2017-2019, Voxel51, Inc.
+Copyright 2017-2020, Voxel51, Inc.
 voxel51.com
 
 Brian Moore, brian@voxel51.com
