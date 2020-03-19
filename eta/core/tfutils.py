d41a9b8ef8b500844e2d5786959e6d59ad221450
brimoor <brimoor@umich.edu>
2020-03-19 14:07:36 -0400

    updating code ownership

--- eta/core/tfutils.py
+++ eta/core/tfutils.py
@@ -5,7 +5,6 @@ Copyright 2017-2020, Voxel51, Inc.
 voxel51.com
 
 Brian Moore, brian@voxel51.com
-Yash Bhalgat, yash@voxel51.com
 '''
 # pragma pylint: disable=redefined-builtin
 # pragma pylint: disable=unused-wildcard-import
