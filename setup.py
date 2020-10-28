564185e4ed2a4d1ee8c881e7743b7a989c6fd7aa
alan <alan@voxel51.com>
2020-10-28 11:07:55 -0400

    Bump to 0.1.11

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ class BdistWheelCustom(bdist_wheel):
 
 setup(
     name="voxel51-eta",
-    version="0.1.10",
+    version="0.1.11",
     description="Extensible Toolkit for Analytics",
     author="Voxel51, Inc.",
     author_email="info@voxel51.com",
