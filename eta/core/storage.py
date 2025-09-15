df96b91a2d37ea9934a8066638fda9e393ebaf80
afoley587 <54959686+afoley587@users.noreply.github.com>
2025-09-15 14:57:23 -0400

    build(actions): AS-898 utilizing trusted publisher workflows (#673)

--- eta/core/storage.py
+++ eta/core/storage.py
@@ -2259,7 +2259,9 @@ class GoogleCloudStorageClient(
             self._client._credentials, impersonated_credentials.Credentials
         ):
             self._signing_credentials = self._client._credentials
-        elif self._is_default_credentials and self._signing_credentials is None:
+        elif (
+            self._is_default_credentials and self._signing_credentials is None
+        ):
             # May need to ensure the client has been used at least once
             # https://gist.github.com/jezhumble/91051485db4462add82045ef9ac2a0ec?permalink_comment_id=3585157#gistcomment-3585157
             _ = self._get_blob(cloud_path)
